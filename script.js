78932aacb0ed93c3f68830db98c12b182a84606b
shamayitasarkar46 <145526018+shamayitasarkar46@users.noreply.github.com>
2023-10-16 15:23:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Frontend Developer", "Designer", "Engineering Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Frontend Developer", "Designer", "Engineering Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
